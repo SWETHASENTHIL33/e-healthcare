e9d73086abd4429e75bf7fe8660400d0228d1656
senthilswetha <119414016+senthilswetha@users.noreply.github.com>
2023-08-18 20:33:28 +0530

    Add files via upload

---
+++ app/asserts/main.ts
@@ -0,0 +1,9 @@
+
+7 lines (4 loc) · 214 Bytes
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
